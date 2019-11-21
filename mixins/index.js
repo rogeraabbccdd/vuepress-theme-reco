e330f0c9363fb70069acec5a2ed258384d940fee
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-11-21 14:33:55 +0800

    Change tag colors

--- mixins/index.js
+++ mixins/index.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     _tagColor () {
       // 红、蓝、绿、橙、灰
-      const tagColorArr = ['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87', '#e15b64', '#f47e60', '#f8b26a', '#f26d6d', '#67cc86', '#fb9b5f', '#3498db']
+      const tagColorArr = ['#8bc34a', '#673ab7', '#f47e60', '#ff9800', '#f44336', '#00abc0', '#2196f3', '#67cc86', '#fb9b5f']
       const index = Math.floor(Math.random() * tagColorArr.length)
       return tagColorArr[index]
     },
